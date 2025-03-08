c3226deb598c1e8b62aba5d915c88faee86f1ef1
tgorman31 <gorman.thomas31@gmail.com>
2025-03-08 16:20:38 +1300

    Updated Elephant class

---
+++ Elephant.cs
@@ -0,0 +1,24 @@
+﻿namespace ElephantSwap;
+
+class Elephant
+{
+    public string Name = "";
+    public int earSize;
+
+    public void WhoAmI()
+    {
+        Console.WriteLine($"My name is {Name}.");
+        Console.WriteLine($"My ears are {earSize} inches tall.\n");
+    }
+
+    public void HearMessage(string message, Elephant whoSaidIt)
+    {
+        Console.WriteLine($"{Name} + heard a message");
+        Console.WriteLine($"{whoSaidIt.Name} said this: {message}");
+    }
+
+    public void SpeakTo(Elephant whoToTalkTo, string message)
+    {
+        whoToTalkTo.HearMessage(message, this);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ElephantSwap;
+
+Elephant lucinda = new Elephant() { Name = "Lucinda", earSize = 33 };
+Elephant lloyd = new Elephant() { Name = "Lloyd", earSize = 40 };
+
+Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+
+while (true)
+{
+	char? enteredValue = Console.ReadKey(true).KeyChar;
+	if (int.TryParse(enteredValue.ToString(), out int value))
+	{
+		Console.WriteLine($"You pressed {value}");
+		if (value == 1)
+		{
+			Console.WriteLine("Calling lloyd.WhoAmI()");
+			lloyd.WhoAmI();
+		} else if (value == 2)
+        {
+            Console.WriteLine("Calling lucinda.WhoAmI()");
+            lucinda.WhoAmI();
+        } else if (value == 3)
+        {
+            Elephant placeholder = lloyd;
+            lloyd = lucinda;
+            lucinda = placeholder;
+            Console.WriteLine("References have been swapped\n");
+
+        }
+    }
+}
